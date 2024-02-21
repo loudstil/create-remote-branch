9eff9e772e8090dd3d057243e0fb0e744b8b666b
Daniel Laettner <daniel.laettner@gmail.com>
2024-02-21 15:18:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ try {
     const branchName =  core.getInput('branch-name');
     const token = core.getInput('token');
 
-    const commitSHA = await getLatestCommitSha(owner, repo, token);
+    const commitSHA = getLatestCommitSha(owner, repo, token);
     createNewBranchIfNotExists(owner, repo, branchName, commitSHA,token);
     
 } catch (error) {
   core.setFailed(error.message);
 }
 
-async function getLatestCommitSha(owner, repo, token) {
+function getLatestCommitSha(owner, repo, token) {
     const octokit = github.getOctokit(token);
     const commit = await octokit.rest.repos.getCommit({
       owner: owner,
